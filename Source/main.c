a42cb9cfc94226d5cf040618137642fd96296a39
InDeviceMex <indevicemex@gmail.com>
2020-06-15 09:28:50 -0500

    Using storage class : Static in global scope and auto in local scope

--- Source/main.c
+++ Source/main.c
@@ -1,11 +1,11 @@
 #include "../Header/main.h"
 
+static int32_t MAIN_s32Favorite=0;
 int main (void)
 {
-    int32_t s32Favorite=0;
-
-    s32Favorite = APP_s32GetFavortiteNumber();
+    auto int jeje=0;
+    jeje = APP_s32GetFavortiteNumber();
     printf("Hello World\n\r");
-    printf("My Favorite Number is: %d\n\r",s32Favorite);
+    printf("My Favorite Number is: %d\n\r",jeje);
     return  MAIN_enSTATUS_OK;
 }
